698409323835a2d3f82423a5f562cbf30005a0a2
Safaa Chouikh <safaachouikh@gmail.com>
2025-03-18 10:12:55 +0000

    Reupdate message

--- server.js
+++ server.js
@@ -1,27 +1,12 @@
-console.log("Server is starting...");
-const express = require("express");
-const { exec } = require("child_process");
-const path = require("path");
-
+const express = require('express');
 const app = express();
-const PORT = 3000;
+const path = require('path');
 
-// Chemin vers vmrun.exe
-const VMRUN_PATH = `"C:\\Program Files (x86)\\VMware\\VMware Workstation\\vmrun.exe"`;
-// Chemin vers ta machine virtuelle
-const VM_PATH = `"C:\\Users\\pc\\OneDrive - OFPPT\\Documents\\Virtual Machines\\Ubuntu 64-bit\\Ubuntu 64-bit.vmx"`;
-
-// Route pour démarrer la VM
-app.get("/start-vm", (req, res) => {
-    const command = `${VMRUN_PATH} -T ws start ${VM_PATH}`;
-
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            return res.status(500).json({ success: false, message: "Erreur lors du démarrage", error: stderr });
-        }
-        res.json({ success: true, message: "Machine virtuelle démarrée avec succès", output: stdout });
-    });
+ 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));  
 });
 
-// Lancer le serveur
-app
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
